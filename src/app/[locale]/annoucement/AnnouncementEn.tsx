7638c748a6a1a67766ec13b75282a9f37e3fda23
Miguel Alvarez <malvarezl@royalresorts.com>
2025-11-18 12:01:19 -0600

    **Add English and Spanish announcement components for Holiday Inn Club Vacations acquisition**

---
+++ src/app/[locale]/annoucement/AnnouncementEn.tsx
@@ -0,0 +1,239 @@
+import React from 'react';
+import Paragraph from "@/components/commons/ui/paragraph";
+
+const AnnouncementEn = () => {
+    return (
+        <>
+            <h3 className={'text-center font-bold text-lg'}>
+                Holiday Inn Club Vacations Acquires Four Resorts in Mexico
+                from Royal Resorts, Marks First International Expansion for the Company
+            </h3>
+            <Paragraph>
+                ORLANDO, FLA. – (May 23, 2023) <a className={"text-[#5897fb]"} href="https://hicv.com/" target="_blank">Holiday Inn Club Vacations
+                Incorporated</a>, a leading vacation ownership company and partner of <a className={"text-[#5897fb]"}
+                href="https://www.ihgplc.com/en/" target="_blank">IHG® Hotels & Resorts</a> (IHG), announced today that
+                it has acquired four scenic beachfront resorts located in Cancun, Playa del Carmen and the Riviera Maya,
+                Mexico from <a className={"text-[#5897fb]"} href="https://www.royalresorts.com/" target="_blank">Royal Resorts®</a>, a pioneer in the
+                Mexican Caribbean tourism industry. The acquisition also includes most of Royal Resorts’ related
+                companies and its corporate operations. These resorts will be Holiday Inn Club Vacations Incorporated’s
+                first properties located outside the United States, further broadening Holiday Inn Club Vacations’ and
+                IHG’s resort footprint and providing guests with increased choice in leisure destinations.</Paragraph>
+
+            <Paragraph>The four award-winning, family-friendly resorts acquired by Holiday Inn Club Vacations
+                Incorporated – The
+                Royal Sands®, The Royal Cancun®, The Royal Haciendas® and Grand Residences by Royal Resorts® –
+                collectively will add more than 850 villas to the company’s growing portfolio of destination resorts.
+                The company expects it will take approximately one year to fully integrate the resorts, and during this
+                process, the resorts will continue to operate normally.</Paragraph>
+
+            <Paragraph>Once complete, IHG Hotels & Resorts customers will be able to book these properties through IHG
+                and enjoy
+                benefits of IHG’s loyalty program – <a className={'text-[#5897fb]'} href="https://www.ihg.com/onerewards/content/us/en/offers/onerewards?cm_mmc=EXTERNAL-_-6C-_-GLO-_-EN-_-LOY-_-LoyaltyEvolution22-_-VANITY"
+                    target="_blank">IHG One Rewards</a> – including the ability to earn and redeem points for hotel
+                stays. These properties will join IHG’s global network of 18 brands and more than 6,000 hotels globally,
+                adding stunning family travel destinations to IHG’s growing brand portfolio.</Paragraph>
+
+            <Paragraph><b>John Staten, President and Chief Executive Officer, Holiday Inn Club Vacations
+                Incorporated</b>,
+                stated: “We couldn’t be more excited about this news and what it means for Holiday Inn Club Vacations,
+                and most importantly, the families that travel with us. This acquisition is aligned with our growth
+                roadmap where growing guest love and our resorts are two interconnected foundational pillars supporting
+                our all-encompassing mission to be the most loved brand in family travel. With four incredible resorts
+                located throughout one of the fastest growing family travel destinations in the world – the scenic
+                Mexican Caribbean – the acquisition brings an entirely new, all-inclusive experience to our owners, Club
+                members and guests.”</Paragraph>
+
+            <Paragraph><b>Keith Barr Chief Executive Officer, IHG Hotels & Resorts</b>, said: “Guests around the world
+                will now
+                have even more exciting choices to add to their travel plans while also enjoying the rich benefits of
+                our IHG One Rewards program for their current and future stays. The addition of these award-winning
+                resorts also demonstrates the strength of the Holiday Inn brand family and, certainly, founder Kemmons
+                Wilson’s unwavering vision to create a better way to vacation lives on today.”</Paragraph>
+
+            <Paragraph><b>Spence Wilson, Board Chairman, Holiday Inn Club Vacations Incorporated</b>, commented: “Since
+                my
+                father started Holiday Inn Club Vacations more than 40 years ago, there have been many significant
+                growth milestones along the way. Few can top today’s announcement—our first expansion outside the United
+                States. I am incredibly proud of our team and look forward to a future that is brighter and even more
+                exciting as we welcome the Royal Resorts team and their owners, members and guests into our
+                family.”</Paragraph>
+
+            <Paragraph><b>Dr. Kemil Rizk, Chief Executive Officer and President of Royal Resorts</b>, added: “We believe
+                that it
+                is a perfect fit, and we could not ask for a better partner to continue our legacy since welcoming our
+                first guests in 1978. Like us, Holiday Inn Club Vacations is a pioneer company with a family-oriented
+                resort collection and a reputation for outstanding service and quality. Holiday Inn Club Vacations is
+                dedicated to providing memorable vacation experiences for families – something that aligns perfectly
+                with the Royal Resorts mission and values, our points based and fixed week vacation ownership
+                memberships and our team’s everyday commitment to quality service.”</Paragraph>
+
+            <Paragraph>The transaction was reviewed and received approval from the Anti-Trust Commission (Comisión
+                Federal de
+                Competencia Economica, COFECE) of the Mexican government. As part of a separate agreement, Tortuga
+                Resorts UK Limited is acquiring the entities which own and operate the remaining Royal Resorts
+                properties—The Royal Islander® and Royal Uno® All Inclusive Resort & Spa.</Paragraph>
+
+            <Paragraph><b>About the award-winning resorts Holiday Inn Club Vacations Incorporated is acquiring from
+                Royal
+                Resorts</b></Paragraph>
+
+            <Paragraph><i>The Royal Sands</i></Paragraph>
+            <Paragraph>Located in Cancun, the 17-acre resort has 340 two-bedroom villas, three pools and a children’s
+                pool, four
+                restaurants, three bars, a fitness center, spa, supervised kids club, activity center, a lagoon-side
+                marina for sailboats and kayaks, a resort store, coffee shop and an on-site travel agency, along with
+                beach access.</Paragraph>
+
+            <Paragraph><i>The Royal Cancun</i></Paragraph>
+            <Paragraph>Located on Cancun’s north shore with views out over the bay and Isla Mujeres, the 7-plus-acre
+                resort has
+                201 two-bedroom villas and two pools, two full-service restaurants and two bars. It also has a fitness
+                center, tennis courts, activity center and supervised kids club along with a travel agency, convenience
+                store and gift shop. The resort has access to a beach that is perfect for children. Other popular
+                activities at the resort are snorkeling, sail boating and kayak trips.</Paragraph>
+
+            <Paragraph><i>The Royal Haciendas</i></Paragraph>
+            <Paragraph>Located in the Riviera Maya, five minutes to the north of Playa del Carmen, the 23-plus acre
+                resort has
+                252 two-bedroom villas with whirlpool tubs on the private balconies and features four pools, four
+                full-service restaurants and five bars. It also has a spa, fitness center, beauty salon, dive center,
+                travel agency, activity center, supervised kids club, children’s pool with an aqua play area, four large
+                whirlpool tubs on the pool deck, tennis courts and a convenience store. The resort is nestled on the
+                shores of a bay in the CoraSol resort community and is within walking distance of a golf
+                course.</Paragraph>
+
+            <Paragraph><i>Grand Residences by Royal Resorts</i></Paragraph>
+            <Paragraph>Also located to the south of Cancun in the tranquil waterfront village of Puerto Morelos, Grand
+                Residences by Royal Resorts offers beachfront luxury. The 13-plus acre resort has 62 villas with two,
+                three or four bedrooms. The spacious, elegantly furnished units have tropical hardwood furniture,
+                state-of-the-art appliances and amenities, private terraces, spa bathrooms and jacuzzis. Some larger
+                units even have private plunge pools and gardens. The resort features four pools, two restaurants, a
+                beach grill and bar, pool bar, a lobby bar café, fitness center, spa, daily activities, supervised kids
+                club, tennis courts and a gourmet food market.</Paragraph>
+
+            <Paragraph>A brief video overview of the resorts Holiday Inn Club Vacations is acquiring can be found
+                here.</Paragraph>
+
+            <br/>
+            <iframe
+                className={"m-auto"}
+                width="560" height="315"
+                src="https://www.youtube.com/embed/zkhGW_ISFu8" title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+
+            </iframe>
+            <br/>
+            <br/>
+            <Paragraph><b>About Holiday Inn Club Vacations Incorporated</b></Paragraph>
+
+            <Paragraph>Encompassing resorts across the United States and the Mexican Caribbean, Holiday Inn Club
+                Vacations Incorporated is a resort, real estate, and travel company with a mission to be the
+                most loved brand in family travel by delivering easy-to-plan, memorable vacation experiences
+                that strengthen families.</Paragraph>
+
+            <Paragraph>Based in Orlando, Fla., the company has been a leader in the vacation ownership industry
+                since 1982, when Holiday Inn® founder Kemmons Wilson debuted the company’s flagship
+                property, Holiday Inn Club Vacations® at Orange Lake Resort next to Orlando’s Walt Disney
+                World® Resort.</Paragraph>
+
+            <Paragraph>Today, the Holiday Inn Club Vacations resort portfolio spans across the United States, and
+                into the Mexican Caribbean, with the company&apos;s international expansion in May 2023.
+                Throughout its history, the company has maintained the core family values true to its
+                founding Wilson family, while aggressively pursuing growth, transforming its member
+                engagement model and building an industry-leading team passionate about the guest
+                experience.</Paragraph>
+
+            <Paragraph><b>About Royal Resorts</b></Paragraph>
+
+            <Paragraph>Founded in 1975 in Cancun, Royal Resorts® is a pioneer company in Mexican Caribbean tourism
+                and in the international vacation ownership industry. It has been welcoming visitors since
+                1978 to its resorts in Cancun and the Riviera Maya where families enjoy a perfect
+                combination of beach beauty, exceptional guest services and hallmark hospitality. Spacious
+                villas are equipped with everything they need for their stay, and they enjoy a wealth of
+                resort amenities for an unforgettable vacation, including daily activities for all ages,
+                kids clubs, non-motorized water sports, pools, gyms and spas and a varied dining scene.</Paragraph>
+
+            <Paragraph>Each resort has its own charm and character, and all have a family ambiance and a spirit of
+                community, with friendships forged between members and employees over the years. The two
+                Riviera Maya resorts – The Royal Haciendas and Grand Residences – have architectural designs
+                and elements that take their inspiration from Mexico’s grand old haciendas. For more
+                information, visit <a className={"text-[#5897fb]"} href="https://www.royalresorts.com/"
+                                      target="_blank">www.royalresorts.com</a>.</Paragraph>
+
+            <Paragraph><b>About IHG Hotels & Resorts</b></Paragraph>
+
+            <Paragraph><a className={"text-[#5897fb]"} href="https://www.ihgplc.com/en/" target="_blank">IHG Hotels & Resorts</a> [LON:IHG,
+                NYSE:IHG (ADRs)] is a global hospitality company, with a purpose to provide True Hospitality
+                for Good.</Paragraph>
+
+            <Paragraph>With a family of 18 hotel brands and <a className={"text-[#5897fb]"}
+                href="https://www.ihg.com/onerewards/content/us/en/offers/onerewards?cm_mmc=EXTERNAL-_-6C-_-GLO-_-EN-_-LOY-_-LoyaltyEvolution22-_-VANITY"
+                target="_blank">IHG One Rewards</a>, one of the world&apos;s largest hotel loyalty programmes,
+                IHG has over 6,000 open hotels in over 100 countries, and more than 1,800 in the development
+                pipeline.</Paragraph>
+
+            <ul className={"list-disc pl-5"}>
+                <li><b>Luxury & Lifestyle: </b> <a className={"text-[#5897fb]"} href="https://www.sixsenses.com/en" target="_blank">Six
+                    Senses Hotels Resorts Spas</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/regent/hotels/us/en/reservation" target="_blank">Regent Hotels
+                    & Resorts</a>, <a className={"text-[#5897fb]"} href="https://www.intercontinental.com/hotels/gb/en/reservation"
+                                      target="_blank">InterContinental Hotels & Resorts</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.vignettecollectionhotels.com/hotels/us/en/reservation"
+                    target="_blank">Vignette Collection</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/kimptonhotels/hotels/us/en/reservation" target="_blank">Kimpton
+                    Hotels & Restaurants, Hotel Indigo</a></li>
+                <li><b>Premium: </b> <a className={"text-[#5897fb]"} href="https://www.ihg.com/voco/hotels/gb/en/reservation"
+                                        target="_blank">voco hotels</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/hualuxe/hotels/gb/en/reservation" target="_blank">HUALUXE
+                    Hotels & Resorts</a>, <a className={"text-[#5897fb]"} href="https://www.ihg.com/crowneplaza/hotels/gb/en/reservation"
+                                             target="_blank">Crowne Plaza Hotels & Resorts</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/evenhotels/hotels/us/en/reservation" target="_blank">EVEN
+                    Hotels</a></li>
+                <li><b>Essentials: </b> <a className={"text-[#5897fb]"} href="https://www.ihg.com/holidayinn/hotels/gb/en/reservation"
+                                           target="_blank">Holiday Inn Hotels & Resorts</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/holidayinnexpress/hotels/gb/en/reservation" target="_blank">Holiday
+                    Inn Express</a>, <a className={"text-[#5897fb]"} href="https://www.ihg.com/avidhotels/hotels/us/en/reservation"
+                                        target="_blank">avid hotels</a></li>
+                <li><b>Suites: </b> <a className={"text-[#5897fb]"} href="https://www.atwellsuites.com/hotels/gb/en/reservation"
+                                       target="_blank">Atwell Suites</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/staybridge/hotels/gb/en/reservation" target="_blank">Staybridge
+                    Suites</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/holidayinnclubvacations/hotels/us/en/reservation"
+                    target="_blank">Holiday Inn Club Vacations</a>, <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/candlewood/hotels/us/en/reservation" target="_blank">Candlewood
+                    Suites</a></li>
+                <li><b>Exclusive Partners: </b> <a className={"text-[#5897fb]"}
+                    href="https://www.ihg.com/content/us/en/iberostar-beachfront-resorts" target="_blank">Iberostar
+                    Beachfront Resorts</a></li>
+            </ul>
+
+            <Paragraph>InterContinental Hotels Group PLC is the Group&apos;s holding company and is incorporated and
+                registered in England and Wales. Approximately 325,000 people work across IHG&apos;s hotels and
+                corporate offices globally.</Paragraph>
+
+            <Paragraph>Visit us online for more about our <a className={"text-[#5897fb]"} href="https://www.ihg.com/hotels/gb/en/reservation"
+                                                             target="_blank">hotels and reservations</a> and <a className={"text-[#5897fb]"}
+                href="https://www.ihg.com/onerewards/content/us/en/offers/onerewards?cm_mmc=EXTERNAL-_-6C-_-GLO-_-EN-_-LOY-_-LoyaltyEvolution22-_-VANITY"
+                target="_blank">IHG One Rewards</a>. To download the new IHG One Rewards app, visit the <a className={"text-[#5897fb]"}
+                href="https://apps.apple.com/us/app/ihg-hotel-deals-rewards/id368217298" target="_blank">Apple
+                App</a> or <a className={"text-[#5897fb]"}
+                href="https://play.google.com/store/apps/details?id=com.ihg.apps.android&pli=1"
+                target="_blank">Google Play</a> stores.</Paragraph>
+
+            <Paragraph>For our latest news, visit our <a className={"text-[#5897fb]"} href="https://www.ihgplc.com/en/news-and-media"
+                                                         target="_blank">Newsroom</a> and follow us on <a className={"text-[#5897fb]"}
+                href="https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHzeXeNsw3BeAAAAYhJpVzQ7hrfRQSMlpz685eDbXDn1LWPTrqMcrPTVXnzMQ-Hmu2JA0eKdmS-W9YpsoIzx-RKg2umbrpa1fsThHBF3PekuvTtAMEA7-NKaGQJq_X_Nfa5M5A=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fihghotels%26resorts%2F"
+                target="_blank">LinkedIn </a>, <a className={"text-[#5897fb]"} href="https://www.facebook.com/IHGCorporate/"
+                                                  target="_blank">Facebook</a> and <a className={"text-[#5897fb]"}
+                href="https://twitter.com/IHGCorporate" target="_blank">Twitter</a>.</Paragraph>
+
+            <Paragraph>Media Contact:<br/>
+                Ashley Pipa, Holiday Inn Club Vacations<br/>
+                apipa@holidayinnclub.com<br/>
+                407.315.8866
+            </Paragraph>
+        </>
+    );
+};
+
+export default AnnouncementEn;
--- src/app/[locale]/annoucement/page.tsx
+++ src/app/[locale]/annoucement/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import AnnouncementEn from "@/app/[locale]/annoucement/AnnouncementEn";
+import AnnouncementEs from "@/app/[locale]/annoucement/AnnouncementEs";
+import {getLocale} from "next-intl/server";
 
-const Page = () => {
+const Page = async () => {
+    const locale = await getLocale();
+
     return (
-        <div>
-            
-        </div>
+        <main className={"my-container pt-[75px]"}>
+            {locale === 'en' && <AnnouncementEn/>}
+            {locale === 'es' && <AnnouncementEs/>}
+        </main>
     );
 };
 
