89d3302e57c3ece29557fa7691328c9158fb17f6
Miguel Alvarez <malvarezl@royalresorts.com>
2025-12-03 16:30:15 -0600

    Comment out standalone output in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-    output: 'standalone',
+    // output: 'standalone',
     /* config options here */
     webpack(config) {
 
