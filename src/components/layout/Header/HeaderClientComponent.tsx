2bfa7296e9d89340b7ffc63c8c9c1e9f0ffd62d2
Miguel Alvarez <malvarezl@royalresorts.com>
2025-12-04 15:42:26 -0600

    Removed unused `ModalMyRoyal` import from HeaderClientComponent in main.

--- src/components/layout/Header/HeaderClientComponent.tsx
+++ src/components/layout/Header/HeaderClientComponent.tsx
@@ -1,5 +1,4 @@
 'use client'
 import dynamic from "next/dynamic";
 
-export const ModalMyRoyal = dynamic(() => import("@/components/commons/shared/my-royal/ModalMyRoyal"), {ssr: false});
 export const BrandsCarousel = dynamic(() => import("@/components/layout/BrandsCarousel"), {ssr: false});
