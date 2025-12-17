b5d0b77ee12e7d01bcffdd7f839b5b51b170326a
Miguel Alvarez <malvarezl@royalresorts.com>
2025-12-17 14:27:10 -0600

    Add structured JSON-LD data for video, resort, and offers to enhance SEO on the home page in `main`.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -22,7 +22,7 @@ const SectionTripadvisor = dynamic(() => import("@/components/pages/home/Section
 
 export default async function Home() {
     const headersList = await headers();
-    const isDesktop =  headersList.get('sec-ch-ua-mobile') === '?0';
+    const isDesktop = headersList.get('sec-ch-ua-mobile') === '?0';
 
     const {home, weddings} = await getMessages();
     const t = await getTranslations('general');
@@ -33,10 +33,115 @@ export default async function Home() {
         getReviews()
     ])
 
+    const makesOffers = offers.filter(offer => offer.rate != null).map((offer) => {
+        return {
+            "@type": "Offer",
+            "availability": "https://schema.org/InStock",
+            "price": offer.rate?.price,
+            "priceCurrency": "USD",
+            "availabilityEnds": offer.travelWindow.end_date,
+            "availabilityStarts": offer.travelWindow.start_date,
+        }
+    })
+
+    const minRate = offers
+        .filter(offer => offer.rate?.price !== null)
+        .reduce((min, offer) => offer.rate?.price != undefined ? offer.rate?.price < min ? offer.rate?.price : min : min, 900000);
+
+    const maxRate = offers
+        .filter(offer => offer.rate?.price !== null)
+        .reduce((max, offer) => offer.rate?.price != undefined ? offer.rate?.price > max ? offer.rate?.price : max : max, 0);
+
+    const jsonDlReviews = reviews.map(review => {
+        return {
+            "@type": "Review",
+            "author": {
+                "@type": "Person",
+                "name": review.traveler_name
+            },
+            "datePublished": review.review_date,
+            "reviewBody": review.review,
+            "name": review.title,
+            "reviewRating": {
+                "@type": "Rating",
+                "bestRating": "5",
+                "ratingValue": "5",
+                "worstRating": "5"
+            }
+        }
+    })
+
+
+    const jsonLdVideo = {
+        "@type": "VideoObject",
+        "name": "Grand Residences Riviera Cancun",
+        "thumbnailUrl": [
+            cdn('/video/home.jpg')
+        ],
+        "uploadDate": "2023-05-31T08:00:00+08:00",
+        "duration": "PT15S",
+        "contentUrl": cdn('/video/home-low.mp4'),
+        "description": tHome('metadescription'),
+    }
+
+
+    const jsonDlResort = {
+        "@context": "http://schema.org/",
+        "@type": "Resort",
+        "name": 'Grand Residences Riviera Cancun',
+        "description": tHome('metadescription'),
+        "image": [
+            cdn('/video/home-banner-new.jpg')
+        ],
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Blvd. El Cid, Unidad 28 Lote 1-01, 77580 Puerto Morelos, Quintana Roo, Mexico",
+            "addressCountry": "Mexico"
+        },
+        "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": "20.827114863410653",
+            "longitude": "-86.89710833173757"
+        },
+        "telephone": '52 99 81 00 06 92',
+        "starRating": {
+            "@type": "Rating",
+            "ratingValue": "5"
+        },
+        "aggregateRating": {
+            "@type": "AggregateRating",
+            "ratingValue": 5,
+            "bestRating": 5,
+            "reviewCount": 2081
+        },
+        "openingHours": "Mo-Su Monday through Sunday, all day",
+        makesOffer: {
+            "@type": "AggregateOffer",
+            "priceCurrency": "USD",
+            "highPrice": maxRate,
+            "lowPrice": minRate,
+            "offerCount": makesOffers.length,
+            'offers': makesOffers
+        },
+        review: jsonDlReviews
+    }
+
     return (
         <main>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(jsonLdVideo).replace(/</g, '\\u003c'),
+                }}
+            />
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(jsonDlResort).replace(/</g, '\\u003c'),
+                }}
+            />
             <section className="relative lg:-mb-10">
-                <div className="aspect-[5/4] md:aspect-[1921/500]">
+                <div className="aspect-5/4 md:aspect-1921/500">
                     <SmartVideo
                         isDesktop={isDesktop}
                         fetchPriority="high"
@@ -49,7 +154,7 @@ export default async function Home() {
 
 
                 <div className="my-container -mt-15 pb-5
-                    lg:absolute lg:w-[450px] lg:bottom-10 lg:right-0 md:mt-0">
+                    lg:absolute lg:w-112.5 lg:bottom-10 lg:right-0 md:mt-0">
                     <Carousel>
                         <div className="bg-[#ffffffe6] p-5 shadow-lg">
                             <CarouselContent className="items-stretch ">
